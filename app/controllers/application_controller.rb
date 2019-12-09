3688064c579872850af7d426334542342eda19a4
nguyentrison2585 <20163116@student.hust.edu.vn>
2019-12-09 20:17:29 +0700

    Chapter 5: Filling in the layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
 class ApplicationController < ActionController::Base
-  def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
-      base_title
-    else
-      page_title + " | " + base_title
-    end
-  end
 end
